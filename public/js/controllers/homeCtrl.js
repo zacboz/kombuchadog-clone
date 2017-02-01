54ed6c4376f5a96602e54ca0767385b52b1efea3
zacboz <zacboz@gmail.com>
2017-01-31 17:46:50 -0700

    finished parallax on other views

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,5 +1,18 @@
 angular.module('kombuchadog')
 .controller('homeCtrl', function($scope, mainSrvc, $stateParams){
 
+  var velocity = 0.4;
+
+  function update(){
+  var pos = $(window).scrollTop();
+  $('.home-header-image').each(function() {
+     var $element = $(this);
+     // subtract some from the height b/c of the padding
+     var height = $element.height()-1730;
+     $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) +  'px');
+    });
+    };
+
+  $(window).bind('scroll', update);
 
 });
--- public/js/controllers/successCtrl.js
+++ public/js/controllers/successCtrl.js
@@ -5,4 +5,19 @@ angular.module('kombuchadog').controller('successCtrl', function($scope, mainSrv
     console.log($scope.adopted);
   });
 
+  var velocity = 0.2;
+
+  function update(){
+  var pos = $(window).scrollTop();
+  $('.success-banner').each(function() {
+     var $element = $(this);
+     // subtract some from the height b/c of the padding
+     var height = $element.height()-1920;
+     $(this).css('backgroundPosition', '65.5% ' + Math.round((height - pos) * velocity) +  'px');
+    });
+    };
+
+  $(window).bind('scroll', update);
+
+
 });
