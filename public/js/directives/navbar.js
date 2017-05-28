840ffea1cea808137dd05d6ae4c81ea21fb69fcd
zacboz <zacboz@gmail.com>
2017-05-27 21:10:06 -0600

    cleaned up mobile nave open and close

--- public/js/directives/navbar.js
+++ public/js/directives/navbar.js
@@ -3,77 +3,37 @@ angular.module('kombuchadog').directive('navbar', () => {
     templateUrl: './views/navbar.html',
     controller: function($state, $rootScope){
 
-
-      var isActive = false;
-
       $('.activate-mobile-menu').on('click', function() {
-      	if (isActive) {
-          $('body').removeClass('mobile-open');
-          $('body').removeClass('routes-open');
-          $('body').removeClass('social-open');
-          $('body').removeClass('hidden-nav');
-          $('body').removeClass('social-right');
-          // $('body').addClass('menu-close');
-          $('body').addClass('hidden-social');
-      	} else {
       		$('body').addClass('mobile-open');
           $('body').addClass('routes-open');
+          $('body').addClass('hidden-social');
           $('body').addClass('social-right');
           $('body').addClass('hidden-nav');
-          $('body').addClass('hidden-social');
-          $('body').removeClass('menu-close');
-      	}
-      	isActive = !isActive;
+          // $('body').removeClass('menu-close');
       });
 
       $('.social-menu').on('click', function() {
-      	if (isActive) {
-          $('body').addClass('mobile-open');
-          $('body').addClass('routes-open');
-          $('body').addClass('hidden-nav');
+          $('body').removeClass('routes-open');
           $('body').removeClass('hidden-social');
-      	} else {
-          // $('body').removeClass('hidden-social');
           $('body').removeClass('social-right');
           $('body').addClass('social-open');
-          $('body').removeClass('routes-open');
-      	}
-      	isActive = !isActive;
       });
 
       $('.back').on('click', function() {
-      	if (isActive) {
-          $('body').addClass('mobile-open');
-          $('body').addClass('social-open');
-          $('body').removeClass('hidden-social');
-          $('body').addClass('hidden-nav');
-          $('body').removeClass('routes-open');
-      	} else {
+          $('body').removeClass('social-open');
           $('body').addClass('social-right');
+          $('body').addClass('hidden-social');
           $('body').addClass('routes-open');
-      	}
-      	isActive = !isActive;
       });
 
       $('.close').on('click', function() {
-      	if (isActive) {
-          $('body').addClass('routes-open');
-          $('body').addClass('social-open');
-          $('body').addClass('mobile-open');
-          // $('body').removeClass('menu-close');
-          $('body').addClass('hidden-nav');
-      	} else {
-          $('body').addClass('hidden-social');
           $('body').removeClass('routes-open');
           $('body').removeClass('social-open');
           $('body').removeClass('social-right');
           $('body').removeClass('hidden-nav');
           $('body').removeClass('mobile-open');
-          // $('body').addClass('menu-close');
-      	}
-      	isActive = !isActive;
+          $('body').addClass('menu-close');
       });
-
 
     }
   };
